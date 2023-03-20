da0649ac0c8b7793d0302c0c9a8caccf9a0fcd14
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-03-20 09:48:22 +0600

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  auto price = 99.99;
+  auto interestRate = 3.67f
+  auto fileSize = 90000L;
+  auto letter = 'a';
+  auto isValid = false;
+  return 0;
+}
